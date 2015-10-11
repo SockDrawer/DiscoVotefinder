931ad9e8cf65075fb5a80b7925cbe694a8dc970d
Andrew Cook <thecourtjester@twelvebaud.net>
2015-10-10 22:21:57 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.Net;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace DiscoVotefinder
+{
+	public static class MainClass
+	{
+		public class Player
+		{
+			public string Name = "";
+			public DateTime LastPost = DateTime.MinValue;
+			public Vote CurrentVote;
+			public List<Vote> VotesAgainst = new List<Vote>();
+			public int Postcount;
+
+			public int Votecount { get { return VotesAgainst.Count((v) => v.UnvotePostNum == 0); } }
+
+			public bool MinusOne { get { return Math.Ceiling(MainClass.Players.Count() / 2.0) - Votecount == 1; } }
+
+			public bool Hammered { get { return Math.Ceiling(MainClass.Players.Count() / 2.0) - Votecount <= 0; } }
+
+			public override string ToString()
+			{
+				if(CurrentVote != null)
+					return string.Format("[Player: {1}({0}) -> {2}]", Votecount, Name, CurrentVote.Target);
+				return string.Format("[Player: {1}({0})]", Votecount, Name);
+			}
+		}
+
+		public class Vote
+		{
+			public string Actor;
+			public string Target;
+			public long VotePostNum;
+			public long UnvotePostNum;
+		}
+
+		public static string Base;
+
+		public static Dictionary<string,Player> Players = new Dictionary<string, Player>();
+
+		public static void Main(string[] args)
+		{
+			JObject config = JObject.Parse(File.ReadAllText("Game.json"));
+			Base = (string) config["auth"]["url"];
+			int topic = (int) config["auth"]["thread"];
+			int skip = (int) config["state"]["startpost"];
+			foreach(string player in config["state"]["players"]) {
+				Player p = new Player() { Name = player };
+				Players[player.ToLower()] = p;
+			}
+
+			foreach(JObject post in GetPosts(topic)) {
+				if((int) post["post_number"] < skip)
+					continue;
+				var actor = ((string) post["username"]).ToLower();
+				if(Players.ContainsKey(actor)) {
+					Players[actor].LastPost = DateTime.Parse((string) post["created_at"]);
+					Players[actor].Postcount++;
+					ProcessActions(post, ProcessAction);
+				}
+			}
+
+			var Quiets = "<table><tr><th>Player</th><th>Posts</th></tr>" + String.Join("",
+				             Players.Values.OrderByDescending(p => p.Postcount).Select(p => string.Format("<tr><td>{0}</td><td>{1}</td>",
+					             p.Name, p.Postcount))) + "</table>";
+
+			var t = new PostTemplate();
+			t.Session = new Dictionary<string, object>();
+			t.Session["Day"] = (int) config["state"]["day"];
+			t.Session["Motd"] = (string) config["state"]["motd"];
+			t.Session["Deadline"] = DateTime.Parse((string) config["state"]["deadline"]);
+			t.Session["Players"] = Players.Values;
+			t.Session["Topic"] = topic;
+
+			t.Initialize();
+			System.Console.Write(t.TransformText());
+
+			System.Diagnostics.Debugger.Break();
+		}
+
+		public static Regex QuoteStripper = new Regex("<aside[^>]+>(?>(?!</?aside).|<aside[^>]+>(?<Depth>)|</aside>(?<-Depth>))*(?(Depth)(?!))</aside>");
+		public static Regex VoteFinder;
+
+		public static void ProcessActions(JObject postdata, Action<string, string, string, int> callback)
+		{
+			if(VoteFinder == null) {
+				var vfsb = new StringBuilder("(?:<h2>(?:##)?|##)(?<action>vote|unvote|kill)(?:</strong>)?(?: (?:<a class=\"mention\" href=\"[^\"]+\">@(?<target>[^<]+)</a>");
+				foreach(var p in Players.Values) {
+					vfsb.Append("|(?<target>");
+					vfsb.Append(p.Name.ToLower());
+					vfsb.Append(")");
+				}
+				vfsb.Append("))?");
+				VoteFinder = new Regex(vfsb.ToString());
+			}
+			string actor = (string) postdata["username"];
+			string post = (string) postdata["cooked"];
+			int postnum = (int) postdata["post_number"];
+			post = QuoteStripper.Replace(post, "").ToLower();
+			var actions = VoteFinder.Matches(post);
+			foreach(Match actionmatch in actions) {
+				string action = actionmatch.Groups["action"].Value;
+				string target = null;
+				if(actionmatch.Groups["target"].Success)
+					target = actionmatch.Groups["target"].Value;
+				callback(actor.ToLower(), action, target, postnum);
+			}
+		}
+
+		public static void ProcessAction(string actor, string action, string target, int postnum)
+		{
+			if(Players.ContainsKey(actor) && Players[actor].CurrentVote != null &&
+			   ("unvote".Equals(action) || ("vote".Equals(action) && !Players[actor].CurrentVote.Target.ToLower().Equals(target)))) {
+				Players[actor].CurrentVote.UnvotePostNum = postnum;
+				Players[actor].CurrentVote = null;
+			}
+			if("vote".Equals(action) && target != null && Players.ContainsKey(actor) && Players.ContainsKey(target) &&
+				(Players[actor].CurrentVote == null || !Players[actor].CurrentVote.Target.ToLower().Equals(target))) {
+				Vote v = new Vote() {
+					Actor = Players[actor].Name,
+					Target = Players[target].Name,
+					VotePostNum = postnum
+				};
+				Players[actor].CurrentVote = v;
+				Players[target].VotesAgainst.Add(v);
+			}
+		}
+
+		public static IEnumerable<JObject> GetPosts(long topic)
+		{
+			string url = string.Format("{0}/t/{1}/posts.json?includeraw=1", Base, topic);
+			var streamjson = (url + "&post_ids[]=0").ToJson();
+			var stream = (JArray) streamjson["post_stream"]["stream"];
+			int i = 0;
+			while(i < stream.Count) {
+				StringBuilder sb = new StringBuilder(url);
+				while(i < stream.Count) {
+					sb.Append("&post_ids[]=");
+					sb.Append((int) stream[i++]);
+					if(i % 100 == 0)
+						break;
+				}
+				var hunk = sb.ToString().ToJson();
+				foreach(JObject post in hunk["post_stream"]["posts"])
+					yield return post;
+			}
+			yield break;
+		}
+
+		public static JObject ToJson(this string url)
+		{
+			System.Threading.Thread.Sleep(2000);
+			return JObject.Load(new JsonTextReader(new StringReader(new WebClient().DownloadString(url))));
+		}
+	}
+}
---
+++ SvgGenerator.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Text;
+
+namespace DiscoVotefinder
+{
+	public static class SvgGenerator
+	{
+		public static double Width = 100.0;
+		public static double Height = 12.0;
+
+		public static string OkFg = "#005600";
+		public static string OkBg = "#FFFFFF"; // not used
+		public static string M1Fg = "#617500";
+		public static string M1Bg = "#B6CF3F";
+		public static string NgFg = "#AC1717";
+		public static string NgBg = "#D74242"; // not used
+		public static string Ovk = "#560000";
+
+
+		public static string GenerateDataUri(int players, int votes){
+			var svg = GenerateSvg(players, votes);
+			var base64 = System.Convert.ToBase64String(Encoding.UTF8.GetBytes(svg));
+			return "data:image/svg+xml;base64," + base64;
+		}
+
+		public static string GenerateSvg(int players, int votes){
+			var sb = new StringBuilder(String.Format("<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"{0:0}\" height=\"{1:0}\">", Width, Height));
+			double target = Math.Ceiling(players / 2.0);
+			if(votes >= target) { //Hammered
+				double overkilllimit = players-target;
+				double overkill = votes - target;
+				sb.Append(string.Format("<rect width=\"100%\" height=\"100%\" fill=\"{0}\" />", NgFg));
+				sb.Append(string.Format("<rect x=\"{1:0.00%}\" width=\"{0:0.00%}\" height=\"100%\" fill=\"{2}\" />", 
+					overkill / overkilllimit, 1.0 - overkill / overkilllimit, Ovk));
+			}
+			else if(votes == target - 1) {
+				sb.Append(string.Format("<rect width=\"100%\" height=\"100%\" fill=\"{0}\" />", M1Bg));
+				sb.Append(string.Format("<rect x=\"{1:0.00%}\" width=\"{0:0.00%}\" height=\"100%\" fill=\"{2}\" />", 
+					votes / target, 1.0 - votes / target, M1Fg));
+
+			}
+			else {
+				//sb.Append(string.Format("<rect width=\"100%\" height=\"100%\" fill=\"{0}\" />", OkBg));
+				sb.Append(string.Format("<rect x=\"{1:0.00%}\" width=\"{0:0.00%}\" height=\"100%\" fill=\"{2}\" />", 
+					votes / target, 1.0 - votes / target, OkFg));
+			}
+			sb.Append("</svg>");
+			return sb.ToString();
+		}
+	}
+}
+
